a5178357b7752e7647d214c064f2c71b29c03a08
camilosky09 <144274662+camilosky09@users.noreply.github.com>
2024-04-14 18:11:18 -0600

    Add files via upload
    Review and user rating system that involves different shop names. Also calculates average rating based on past reviews. MAUI integrated needed to be switched to an XAML.cs file.

---
+++ ReviewManager.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MechanicFinder
+{
+    // Class to manage reviews and ratings for mechanic shops
+    public class ReviewManager
+    {
+        private List<Review> reviews;
+
+        public ReviewManager()
+        {
+            reviews = new List<Review>();
+        }
+
+        public void AddReview(Review review)
+        {
+            reviews.Add(review);
+        }
+
+        public List<Review> GetReviewsForMechanicShop(string mechanicShopName)
+        {
+            return reviews.FindAll(review => review.MechanicShopName.Equals(mechanicShopName, StringComparison.OrdinalIgnoreCase));
+        }
+
+        // Method to calculate the average rating for a mechanic shop
+        public double GetAverageRatingForMechanicShop(string mechanicShopName)
+        {
+            List<Review> shopReviews = GetReviewsForMechanicShop(mechanicShopName);
+            if (shopReviews.Count == 0)
+            {
+                return 0; // No reviews yet
+            }
+
+            int totalRating = 0;
+            foreach (var review in shopReviews)
+            {
+                totalRating += review.Rating;
+            }
+
+            return (double)totalRating / shopReviews.Count;
+        }
+    }
+
+}
