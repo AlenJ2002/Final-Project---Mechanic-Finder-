b1c5f1456c464c8055adc083021af0f3fe771a33
AlenJ2002 <156727145+AlenJ2002@users.noreply.github.com>
2024-04-15 00:43:58 -0600

    Add files via upload

---
+++ App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace MechanicFinder
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ AppShell.xaml.cs
@@ -0,0 +1,14 @@
+﻿// AppShell.xaml.cs
+using Microsoft.Maui.Controls;
+
+namespace MechanicFinder
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+            // Registering routes is done in XAML in this case, but can also be done here.
+        }
+    }
+}
